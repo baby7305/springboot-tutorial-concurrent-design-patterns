3cd3e36969807c76bc6de33e97422940dbd5a932
baby7305 <songhaibei7305@163.com>
2019-07-25 15:35:39 +0800

    guarded suspension、保护性暂挂设计模式范例
    LinkedList加了final，模拟Queue的读写一致性。

---
+++ src/main/java/com/example/design/guardedSuspension/Request.java
@@ -0,0 +1,18 @@
+package com.example.design.guardedSuspension;
+
+public class Request {
+	private final String name;
+
+	public Request(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String toString() {
+		return "[ Request " + name + " ]";
+	}
+}
+
