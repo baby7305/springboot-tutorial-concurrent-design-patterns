afa1e612c69814228119ae6ef30f88de5de7df25
baby7305 <songhaibei7305@163.com>
2019-08-07 14:11:03 +0800

    Thread-pool 线程池设计模式范例
    Channel类是实质上的线程池，内部运用了Producer-Consumer、Guard Suspension模式，死循环线程在不停空转。

---
+++ src/main/java/com/example/design/threadPool/Main.java
@@ -0,0 +1,12 @@
+package com.example.design.threadPool;
+
+public class Main {
+    public static void main(String[] args) {
+        Channel channel = new Channel(5);   // 工人线程的个数
+        channel.startWorkers();
+        new ClientThread("Alice", channel).start();
+        new ClientThread("Bobby", channel).start();
+        new ClientThread("Chris", channel).start();
+    }
+}
+
