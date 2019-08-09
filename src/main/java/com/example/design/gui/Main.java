5b6526af7ff72d139e5a01ec551790673f5dac35
baby7305 <songhaibei7305@163.com>
2019-08-09 10:10:04 +0800

    gui程序应用
    界面中点击countUp按钮，每隔1秒显示数字0...9

---
+++ src/main/java/com/example/design/gui/Main.java
@@ -0,0 +1,9 @@
+package com.example.design.gui;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName() + ":BEGIN");
+        new MyFrame();
+        System.out.println(Thread.currentThread().getName() + ":END");
+    }
+}
