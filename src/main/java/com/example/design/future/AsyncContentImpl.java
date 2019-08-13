b12ffb50a26e33e0de2db09e9083c2595827ba99
baby7305 <songhaibei7305@163.com>
2019-08-13 11:21:39 +0800

    用FutureTask多线程改造

---
+++ src/main/java/com/example/design/future/AsyncContentImpl.java
@@ -0,0 +1,23 @@
+package com.example.design.future;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+class AsyncContentImpl extends FutureTask<SyncContentImpl> implements Content {
+    public AsyncContentImpl(Callable<SyncContentImpl> callable) {
+        super(callable);
+    }
+
+    public byte[] getBytes() {
+        byte[] bytes = null;
+        try {
+            bytes = get().getBytes();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return bytes;
+    }
+}
--- src/main/java/com/example/design/future/Retriever.java
+++ src/main/java/com/example/design/future/Retriever.java
@@ -1,7 +1,19 @@
 package com.example.design.future;
 
+import java.util.concurrent.Callable;
+
 public class Retriever {
-    public static Content retrieve(String urlstr) {
-        return new SyncContentImpl(urlstr);
+    public static Content retrieve(final String urlstr) {
+        AsyncContentImpl future = new AsyncContentImpl(
+                new Callable<SyncContentImpl>() {
+                    public SyncContentImpl call() {
+                        return new SyncContentImpl(urlstr);
+                    }
+                }
+        );
+
+        new Thread(future).start();
+
+        return future;
     }
 }
