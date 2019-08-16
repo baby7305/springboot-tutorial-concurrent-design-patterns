93714dd8077384ee0db2de4d9735aeb11b2b6894
baby7305 <songhaibei7305@163.com>
2019-08-16 09:53:14 +0800

    运用java.util.concurrent.ThreadFactory接口

--- src/main/java/com/example/design/threadPerMessage/Host.java
+++ src/main/java/com/example/design/threadPerMessage/Host.java
@@ -1,11 +1,18 @@
 package com.example.design.threadPerMessage;
 
+import java.util.concurrent.ThreadFactory;
+
 public class Host {
     private final Helper helper = new Helper();
+    private final ThreadFactory threadFactory;
+
+    public Host(ThreadFactory threadFactory) {
+        this.threadFactory = threadFactory;
+    }
 
     public void request(final int count, final char c) {
         System.out.println("    request(" + count + ", " + c + ") BEGIN");
-        new Thread(
+        threadFactory.newThread(
                 new Runnable() {
                     public void run() {
                         helper.handle(count, c);
--- src/main/java/com/example/design/threadPerMessage/Main.java
+++ src/main/java/com/example/design/threadPerMessage/Main.java
@@ -1,9 +1,17 @@
 package com.example.design.threadPerMessage;
 
+import java.util.concurrent.ThreadFactory;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("main BEGIN");
-        Host host = new Host();
+        Host host = new Host(
+                new ThreadFactory() {
+                    public Thread newThread(Runnable r) {
+                        return new Thread(r);
+                    }
+                }
+        );
         host.request(10, 'A');
         host.request(20, 'B');
         host.request(30, 'C');
